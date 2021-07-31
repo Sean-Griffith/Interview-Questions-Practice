1937485ca4d42188104be9fa26b663277970fbbe
Sean-Griffith <DarkmatterV2@gmail.com>
2021-07-30 17:55:42 -0700

    threeInOne datastructure start

--- 3 Stacks and Queues/threeInOne.cpp
+++ 3 Stacks and Queues/threeInOne.cpp
@@ -2,12 +2,12 @@
     Describe how you could use a single array to implement three stacks.
 */
 #include <iostream>
+#include "TripleArrayStack.h"
 using namespace std;
 
 int main(){
-    int size1, size2, size3;
-    size1 = size2 = size3 = 0;
-    int* tripleStack = new int[size1+size2+size3];
-
+    TripleArrayStack<int> tripleStack1;
+    TripleArrayStack<int> tripleStack2(tripleStack1);
+    tripleStack1 = tripleStack1;
     return 1;
 }
--- 3 Stacks and Queues/tripleArrayStack.h
+++ 3 Stacks and Queues/tripleArrayStack.h
@@ -15,6 +15,9 @@ class TripleArrayStack {
         TripleArrayStack& operator=(const TripleArrayStack& other);
         ~TripleArrayStack();
 
+        void Push(int stackNum);
+        DataType Pop(int stackNum);
+
     private:
         int m_size1, m_size2, m_size3;
         int* m_array;
@@ -22,7 +25,45 @@ class TripleArrayStack {
 
 template <typename DataType>
 TripleArrayStack<DataType>::TripleArrayStack(){
-    
+    m_size1 = m_size2 = m_size3 = 0;
+    m_array = new int[m_size1 + m_size2 + m_size3];
+}
+
+template <typename DataType>
+TripleArrayStack<DataType>::TripleArrayStack(const TripleArrayStack& other){
+    m_size1 = other.m_size1;
+    m_size2 = other.m_size2;
+    m_size3 = other.m_size3;
+    m_array = new int[m_size1 + m_size2 + m_size3];
+
+    int totalSize = m_size1 + m_size2 + m_size3;
+    for(int i = 0; i < totalSize; i++){
+        m_array[i] = other.m_array[i];
+    }
+}
+
+template <typename DataType>
+TripleArrayStack<DataType>& TripleArrayStack<DataType>::operator=(const TripleArrayStack& other){
+    if(&other == this){
+        return *this;
+    }
+
+    m_size1 = other.m_size1;
+    m_size2 = other.m_size2;
+    m_size3 = other.m_size3;
+    m_array = new int[m_size1 + m_size2 + m_size3];
+
+    int totalSize = m_size1 + m_size2 + m_size3;
+    for(int i = 0; i < totalSize; i++){
+        m_array[i] = other.m_array[i];
+    }
+
+    return *this;
+}
+
+template <typename DataType>
+TripleArrayStack<DataType>::~TripleArrayStack(){
+    delete m_array;
 }
 
 #endif
