cd4862b28782b4cd7f8425553ba78648746c311d
Sean-Griffith <DarkmatterV2@gmail.com>
2021-08-07 13:07:54 -0700

    Implementation for a queue using two stacks

--- 3 Stacks and Queues/StackQueue.h
+++ 3 Stacks and Queues/StackQueue.h
@@ -12,29 +12,72 @@ class StackQueue {
         StackQueue<DataType>& operator=(const StackQueue& other);
         ~StackQueue();
 
+        void Add(DataType data);
+        DataType Remove();
+        DataType Peek();
+
+        bool isEmpty();
+
     private:
-        Stack<DataType> m_stack1;
-        Stack<DataType> m_stack2;
+        Stack<DataType>* m_inStack;
+        Stack<DataType>* m_outStack;
 };
 
 template <typename DataType>
 StackQueue<DataType>::StackQueue(){
-    
+    m_inStack = new Stack<DataType>;
+    m_outStack = new Stack<DataType>;
 }
 
 template <typename DataType>
 StackQueue<DataType>::StackQueue(const StackQueue& other){
-    
+    m_inStack = other->m_inStack;
+    m_outStack = other->m_outStack;
 }
 
 template <typename DataType>
 StackQueue<DataType>& StackQueue<DataType>::operator=(const StackQueue<DataType>& other){
-    
+    if(&other == this){
+        return *this;
+    }
+    m_inStack = other->m_inStack;
+    m_outStack = other->m_outStack;
+    return *this;
 }
 
 template <typename DataType>
 StackQueue<DataType>::~StackQueue(){
-    
+    delete(m_inStack);
+    delete(m_outStack);
+}
+
+template <typename DataType>
+void StackQueue<DataType>::Add(DataType data){
+    while(!m_outStack->isEmpty()){
+        m_inStack->Push(m_outStack->Pop());
+    }
+    m_inStack->Push(data);
+}
+
+template <typename DataType>
+DataType StackQueue<DataType>::Remove(){
+    while(!m_inStack->isEmpty()){
+        m_outStack->Push(m_inStack->Pop());
+    }
+    return m_outStack->Pop();
+}
+
+template <typename DataType>
+DataType StackQueue<DataType>::Peek(){
+    while(!m_inStack->isEmpty()){
+        m_outStack->Push(m_inStack->Pop());
+    }
+    return m_outStack->Peek();
+}
+
+template <typename DataType>
+bool StackQueue<DataType>::isEmpty(){
+    return true;
 }
 
 #endif
--- 3 Stacks and Queues/queueViaStacks.cpp
+++ 3 Stacks and Queues/queueViaStacks.cpp
@@ -6,6 +6,14 @@
 using namespace std;
 
 int main(){
-
+    StackQueue<int> example1;
+    //example1.Remove();
+    example1.Add(1);
+    example1.Add(2);
+    example1.Add(3);
+    example1.Add(4);
+    cout << example1.Remove() << endl;
+    example1.Add(5);
+    cout << example1.Remove() << endl;
     return 1;
 }
