19315b3a8bf30b3748e6a6c78d68dce45c9e5aa5
Sean-Griffith <DarkmatterV2@gmail.com>
2021-08-18 10:55:38 -0700

    sortStack solution

--- 3 Stacks and Queues/sortStack.cpp
+++ 3 Stacks and Queues/sortStack.cpp
@@ -5,10 +5,48 @@
     The stack supports the following operations: push, pop, peek, and isEmpty.
 */
 #include <iostream>
-#include "SortStack.h"
+#include "Stack.h"
 using namespace std;
 
-int main(){
+Stack<int> sortStack(Stack<int>& unsorted);
 
+int main(){
+    Stack<int> example;
+    example.Push(3);
+    example.Push(8);
+    example.Push(13);
+    example.Push(4);
+    example.Push(1);
+    example.Push(0);
+    sortStack(example).ShowStructure();
     return 1;
+}
+
+Stack<int> sortStack(Stack<int>& unsorted){
+
+    if(unsorted.isEmpty()){
+        return unsorted;
+    }
+    
+    Stack<int> sorted;
+    int temp = unsorted.Pop();
+
+    while(!unsorted.isEmpty()){
+        if(sorted.isEmpty()){
+            sorted.Push(temp);
+            temp = unsorted.Pop();
+        } else {
+            if(sorted.Peek() > temp){
+                // Pop the element and move to unsorted stack
+                unsorted.Push(sorted.Pop());
+            } else {
+                sorted.Push(temp);
+                temp = unsorted.Pop();
+            }
+        }
+    }
+    while(!sorted.isEmpty()){
+        unsorted.Push(sorted.Pop());
+    }
+    return unsorted;
 }
