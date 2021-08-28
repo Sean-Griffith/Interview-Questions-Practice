0392385b23636043b556ce751a71cc7cb7a084bc
Sean-Griffith <DarkmatterV2@gmail.com>
2021-08-28 15:10:39 -0700

    BST insert, showStructure implementations

--- 4 Trees and Graphs/BST.h
+++ 4 Trees and Graphs/BST.h
@@ -15,7 +15,7 @@ class BST {
 
         void Insert(DataType value);
         DataType Delete(DataType value);
-        bool Search(DataType value);
+        bool Search(DataType value) const;
 
         void Clear();
 
@@ -23,16 +23,21 @@ class BST {
         void InOrder() const;
         void PostOrder() const;
 
-    private:
+        void ShowStructure();
+    protected:
         class BinaryNode{
-            BinaryNode(DataType data, BinaryNode* left=NULL, BinaryNode* right=NULL){
-                
-            }
-            BinaryNode* m_left;
-            BinaryNode* m_right;
-            DataType m_data;
+            public:
+                BinaryNode(DataType data, BinaryNode* left=NULL, BinaryNode* right=NULL){
+                    m_data = data;
+                    m_left = left;
+                    m_right = right;
+                }
+                BinaryNode* m_left;
+                BinaryNode* m_right;
+                DataType m_data;
         };
-
+        void InsertHelper(DataType value, BinaryNode*& currNode);
+        void ShowHelper(BinaryNode* currNode, int depth);
         BinaryNode* m_root;
 };
 
@@ -65,7 +70,20 @@ void BST<DataType>::Clear(){
 
 template <typename DataType>
 void BST<DataType>::Insert(DataType value){
+    InsertHelper(value, m_root);
+}
 
+template <typename DataType>
+void BST<DataType>::InsertHelper(DataType value, BinaryNode*& currNode){
+    if(currNode == NULL){
+        currNode = new BinaryNode(value);
+        return;
+    }
+    if(currNode->m_data >= value){
+        InsertHelper(value, currNode->m_left);
+    } else {
+        InsertHelper(value, currNode->m_right);
+    }
 }
 
 template <typename DataType>
@@ -74,13 +92,13 @@ DataType BST<DataType>::Delete(DataType value){
 }
 
 template <typename DataType>
-bool BST<DataType>::Search(DataType value){
-
+bool BST<DataType>::Search(DataType value) const {
+    return false;
 }
 
 template <typename DataType>
 void BST<DataType>::PreOrder() const {
-
+    
 }
 
 template <typename DataType>
@@ -91,6 +109,41 @@ void BST<DataType>::InOrder() const {
 template <typename DataType>
 void BST<DataType>::PostOrder() const {
 
+}
+
+template <typename DataType>
+void BST<DataType>::ShowStructure(){
+    if(m_root){
+        cout << "BST Structure" << endl;
+        ShowHelper(m_root, 0);
+        cout << endl;
+    } else {
+        cout << "Empty BST" << endl;
+    }
+}
+
+template <typename DataType>
+void BST<DataType>::ShowHelper(BinaryNode* currNode, int depth){
+    if(!currNode){
+        return;
+    }
+    ShowHelper(currNode->m_right, depth+1);
+    // Tab over to current depth
+    for(int i = 0; i < depth; i++){
+        cout << "\t";
+    }
+    // Output current node value
+    cout << " " << currNode->m_data;
+    // Output lines to child nodes (if any)
+    if(currNode->m_right && currNode->m_left){
+        cout << "<";
+    } else if(currNode->m_right){
+        cout << "/";
+    } else if(currNode->m_left){
+        cout << "\\";
+    }
+    cout << endl;
+    ShowHelper(currNode->m_left, depth+1);
 }
 
 #endif
--- 4 Trees and Graphs/minimalTree.cpp
+++ 4 Trees and Graphs/minimalTree.cpp
@@ -8,7 +8,19 @@
 #include "BST.h"
 using namespace std;
 
+BST<int> ArrayToBST(const int* arr, int size);
+
 int main(){
     int arr[7] = {-44, 13, 17, 32, 51, 88, 201};
+    BST<int> solution = ArrayToBST(arr, 7);
+    solution.ShowStructure();
     return 1;
+}
+
+BST<int> ArrayToBST(const int* arr, int size){
+    BST<int> myTree;
+    for(int i = 0; i < size; i++){
+        myTree.Insert(arr[i]);
+    }
+    return myTree;
 }
